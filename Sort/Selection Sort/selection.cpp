b7ad10d554e41eb03c987cfc114a2af30bca5556
omkara18 <omkarshelke1125@gmail.com>
2021-12-22 23:50:07 +0530

    Selection Sort

---
+++ Sort/Selection Sort/selection.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+#define MAX 1000
+int main()
+{   int arr[MAX], cnt,i,j,min,loc;
+    cout<<"Enter Size Of Array :: ";
+    cin>>cnt;
+    cout<<"Enter Array Element ";
+    for(int i=0;i<cnt;i++)
+    {
+        cin>>arr[i];
+    }
+    for(i=0;i<cnt-1;i++)
+    {
+        for(j=0;j<cnt;j++)
+        {
+            min=arr[j];
+            loc=j;
+            if(a[j]<a[j+1])
+            {
+                a[j]
+            }
+        }
+    }
+    return 0;
+}
